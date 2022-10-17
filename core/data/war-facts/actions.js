05bf8f5aee8dcdf17a6f73538c87b9ed77928449
Matteo Giusti <suikofire@gmail.com>
2022-10-17 18:47:44 +0200

    Aggiunge dati

--- core/data/war-facts/actions.js
+++ core/data/war-facts/actions.js
@@ -4,5 +4,8 @@ module.exports = [
     'entra in guerra contro',
     'si complimenta ufficialmente con',
     'denigra ufficialmente',
-    'lancia una testata nucleare contro'
+    'lancia una testata nucleare contro',
+    'annuncia di odiare',
+    'ringrazia',
+    'invia le proprie truppe contro'
 ];
