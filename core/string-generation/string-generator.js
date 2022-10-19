48c93ce287583cf27d798b768c6d6450f00a0226
Matteo Giusti <suikofire@gmail.com>
2022-10-19 22:14:13 +0200

    Ottimizza string generator

--- core/string-generation/string-generator.js
+++ core/string-generation/string-generator.js
@@ -1,6 +1,6 @@
 const {pickRandomFromArray, randomTrue, capitalize, removeDoubleSpaces} = require('../utilities');
 
-const TOKEN_REGEX = /({{\??[\w|\sàèéìòù',]+}})/g;
+const TOKEN_REGEX = /({{\??.+}})/g;
 
 module.exports = class StringGenerator
 {
@@ -52,17 +52,12 @@ module.exports = class StringGenerator
         let generated = this.pickRandomFormat();
         let iteration = 0;
 
-        while(this.hasReplaceableTokens(generated) && iteration < this.getMaxIterations()) {
+        while(StringGenerator.hasReplaceableTokens(generated) && iteration < this.getMaxIterations()) {
             iteration += 1;
             generated = this.replaceTokens(generated);
         }
 
         return capitalize(removeDoubleSpaces(generated.trim()));
-    }
-
-    replaceTokens(format)
-    {
-        return format.replace(TOKEN_REGEX, (match, token) => this.replaceToken(token));
     }
 
     pickRandomFormat()
@@ -72,7 +67,8 @@ module.exports = class StringGenerator
 
     replaceToken(token)
     {
-        token = token.replace(/{{|}}/g, '');
+        token = token.trim();
+        token = token.substring(2, token.length - 2);
 
         if(token.includes('?')) {
             if(!randomTrue()) {
@@ -92,8 +88,85 @@ module.exports = class StringGenerator
         return pickRandomFromArray(set);
     }
 
-    hasReplaceableTokens(checkedString)
+    replaceTokens(format, iteraction = 0)
+    {
+        if(iteraction > this.maxIterations) {
+            return format;
+        }
+    
+        const tokens = StringGenerator.getTokens(format);
+        tokens.reverse()
+
+        for(const token of tokens) {
+            const before = format.slice(0, token.start);
+            const after = format.slice(token.end);
+            const replaced = this.replaceToken(token.token);
+            format = `${before}${replaced}${after}`;
+        }
+    
+        if(!StringGenerator.hasReplaceableTokens(format)) {
+            return format;
+        }
+
+        return this.replaceTokens(format, iteraction + 1);
+    }
+
+    static hasReplaceableTokens(checkedString)
     {
         return !!checkedString.match(TOKEN_REGEX);
+    }
+
+    static getTokens(checkedString)
+    {
+        let depths = [];
+        let depth = -1;
+    
+        let i = 0;
+    
+        while(i < checkedString.length) {
+    
+            if(checkedString.charAt(i) == '{' && checkedString.charAt(i + 1) == '{') {
+                
+                depth += 1;
+                const token = {
+                    start : i,
+                    end : null,
+                    token : null
+                }
+    
+                if(!depths[depth]) {
+                    depths[depth] = [];
+                }
+    
+                depths[depth].push(token);
+                
+                i += 1;
+    
+            } else if(checkedString.charAt(i) == '}' && checkedString.charAt(i + 1) == '}') {
+                
+                if(depths[depth]) {
+    
+                    if(depth < 0) {
+                        depth = 0;
+                    }
+        
+                    const token = depths[depth][depths[depth].length - 1];
+                    token.end = i + 2;
+                    token.token = checkedString.substring(token.start, token.end)
+                    
+                    depths[depth][depths[depth].length - 1] = token;
+        
+                    depth -= 1;
+                    
+                }
+
+                i += 1;
+                
+            }
+    
+            i += 1;
+        }
+    
+        return depths.find(tokens => tokens.filter(token => !!token.token).length > 0) || [];
     }
 }
