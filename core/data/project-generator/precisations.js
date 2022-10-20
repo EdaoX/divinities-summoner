b2c4d89f7b81db220398be9f086ec6671c78c4cf
Matteo Giusti <suikofire@gmail.com>
2022-10-20 12:03:00 +0200

    Aggiunge dati a generatore progetto

--- core/data/project-generator/precisations.js
+++ core/data/project-generator/precisations.js
@@ -1,4 +1,14 @@
 module.exports = [
     "{{Attenzione|Occhio|Fai a modo}} perché nel tuo ultimo progetto {{se|quando}} {{T}} {{A}}",
-    "Non perderci tanto tempo {{?che c'è altra roba da fare}}"
+    "Non perderci tanto tempo {{?che c'è altra roba da fare}}",
+    'Fammelo tabellare',
+    'Niente robe javascriptose',
+    'Non usare {{?assolutamente|per nessuna ragione}} i cookie',
+    'Tanto sono 5 minuti',
+    'Lo voglio semplice',
+    'Deve essere solo HTML5',
+    'Però senza perderci tanto tempo',
+    'Ti ho mandato già la libreria {{?da usare}} io. {{?Studiatela che poi si fa}}',
+    "C'è già {{fatto|bell'e pronto}}, basta che lo copi",
+    'Mi raccomando aggiungi gli indici al db che non ci sono stati messi'
 ];
