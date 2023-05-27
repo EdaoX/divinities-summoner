129b74073622a041c4a7e48f3f53acaf8a0cab58
Matteo Giusti <suikofire@gmail.com>
2023-05-27 12:05:09 +0200

    Separa rq (comando senza filtri) e rqs (comando senza nsfw)

--- core/commands/quote.js
+++ core/commands/quote.js
@@ -9,10 +9,20 @@ module.exports.commands = [];
 
 const command = 'rq';
 const handler = async ctx => {
-    const { data : { quote : { body, author } = { } } = {} } = await axios.get(randomQuoterUrl);
+    const { data : { quote : { body, author } = { } } = {} } = await axios.get(`${randomQuoterUrl}?nsfw=true`);
     if(body && author) {
         await ctx.replyWithMarkdownV2(`${escapeForMarkdown(body)}\n\n_\\- ${escapeForMarkdown(author)}_`);
     }
 };
 const helpText = createHelpText(command, 'Visualizza una fantastica citazione');
 module.exports.commands.push(createCommand(command, handler, helpText));
+
+const sfwCommand = 'rqs';
+const sfwHandler = async ctx => {
+    const { data : { quote : { body, author } = { } } = {} } = await axios.get(randomQuoterUrl);
+    if(body && author) {
+        await ctx.replyWithMarkdownV2(`${escapeForMarkdown(body)}\n\n_\\- ${escapeForMarkdown(author)}_`);
+    }
+};
+const sfwHelpText = createHelpText(sfwCommand, 'Visualizza una fantastica citazione adatta anche alle famiglie');
+module.exports.commands.push(createCommand(sfwCommand, sfwHandler, sfwHelpText));
